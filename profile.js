4dd1f9d955ed4439c8796f51284aae585ac9f2c6
unknown <emilianomartindominguezfrese@gmail.com>
2022-09-19 19:13:10 -0300

    left unsaved profile.js file

--- profile.js
+++ profile.js
@@ -1,22 +1,23 @@
-const userName = window.location.search.substring(1)
-const closeButton = document.querySelector('#close')
+const userName = window.location.search.substring(1);
+const closeButton = document.querySelector("#close");
 const modal = document.querySelector("#modal");
-const welcomeMessage = document.querySelector('#welcome')
+const welcomeMessage = document.querySelector("#welcome");
 
-welcomeMessage.innerHTML += ` ${userName}`
+welcomeMessage.innerHTML += ` ${userName}`;
 
 const endSession = setTimeout(() => {
-    window.location.replace('./index.html')
+  window.location.replace("./index.html");
 }, 5000);
 
 function extendSession() {
-    clearTimeout(endSession)
-    modal.classList.toggle("display-none")
+  clearTimeout(endSession);
+  modal.classList.toggle("display-none");
 }
 
-const addButton = document.querySelector('#add')
+const addButton = document.querySelector("#add");
 
-addButton.addEventListener('click', extendSession)
+addButton.addEventListener("click", extendSession);
 
-
-closeButton.addEventListener('click', () => modal.classList.toggle('display-none'))
+closeButton.addEventListener("click", () =>
+  modal.classList.toggle("display-none")
+);
