e46572733a05ca1e62acd8e828a81e4be5f62a19
unknown <emilianomartindominguezfrese@gmail.com>
2022-09-19 19:12:33 -0300

    added close modal functionality

--- profile.js
+++ profile.js
@@ -1,17 +1,22 @@
 const userName = window.location.search.substring(1)
-
+const closeButton = document.querySelector('#close')
+const modal = document.querySelector("#modal");
 const welcomeMessage = document.querySelector('#welcome')
 
 welcomeMessage.innerHTML += ` ${userName}`
 
-const test = setTimeout(() => {
+const endSession = setTimeout(() => {
     window.location.replace('./index.html')
 }, 5000);
 
 function extendSession() {
-    clearTimeout(test)
+    clearTimeout(endSession)
+    modal.classList.toggle("display-none")
 }
 
-const button = document.querySelector('#add')
+const addButton = document.querySelector('#add')
 
-button.addEventListener('click', extendSession)
+addButton.addEventListener('click', extendSession)
+
+
+closeButton.addEventListener('click', () => modal.classList.toggle('display-none'))
